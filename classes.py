7029d089f4fdbc3a8fccae607cd0102338ef05a0
manzhos.mihail <manzhos.mihail@mail.ru>
2024-11-20 00:47:17 +0300

    Update classes again

--- classes.py
+++ classes.py
@@ -16,46 +16,46 @@ class User:
             "surname":self.surname,
         }
         return data
-    def features(self) -> None:
-        print(f"{self.network_name}: Основные функции социальной сети.")
+    def features(self) -> str:
+        return(f"{self.network_name}: Основные функции социальной сети.")
 class Instagram(User):
     def __init__(self,id: int,name:str,surname:str) -> None:
          super().__init__(id,name,surname)
-    def features(self) -> None:
-        print(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Публикация фото и видео, Stories(временные публикации), Reels(короткие видео), IGTV(длинные видео), Прямые эфиры")
+    def features(self) -> str:
+        return(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Публикация фото и видео, Stories(временные публикации), Reels(короткие видео), IGTV(длинные видео), Прямые эфиры")
 class Telegram(User):
     def __init__(self,id: int,name:str,surname:str) -> None:
          super().__init__(id,name,surname)
-    def features(self) -> None:
-        print(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Чаты с шифрованием, Группы, Каналы, Боты, Приватные сообщения и секретные чаты")
+    def features(self) -> str:
+        return(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Чаты с шифрованием, Группы, Каналы, Боты, Приватные сообщения и секретные чаты")
 class Tiktok(User):
     def __init__(self,id: int,name:str,surname:str) -> None:
          super().__init__(id,name,surname)
-    def features(self) -> None:
-        print(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Короткие видео с музыкой и фильтрами, Вирусные челленджи, Duet и Stitch (взаимодействие с другими видео), Алгоритм персонализированных рекомендаций, Прямые трансляции")
+    def features(self) -> str:
+        return(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Короткие видео с музыкой и фильтрами, Вирусные челленджи, Duet и Stitch (взаимодействие с другими видео), Алгоритм персонализированных рекомендаций, Прямые трансляции")
 class Vkontakte(User):
     def __init__(self,id: int,name:str,surname:str) -> None:
          super().__init__(id,name,surname)
-    def features(self) -> None:
-        print(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Публикация фото, видео, музыки, Ленты новостей, Группы и сообщества, Видео трансляции и стримы, Магазины и оплата через приложение, Истории")
+    def features(self) -> str:
+        return(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Публикация фото, видео, музыки, Ленты новостей, Группы и сообщества, Видео трансляции и стримы, Магазины и оплата через приложение, Истории")
 class Snapchat(User):
     def __init__(self,id: int,name:str,surname:str) -> None:
          super().__init__(id,name,surname)
-    def features(self) -> None:
-        print(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Секретные фото и видео, Snap Map (карта местоположений), Линзы (фильтры и эффекты), Истории, Прямые трансляции, Челленджи и игры с друзьями")
+    def features(self) -> str:
+        return(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Секретные фото и видео, Snap Map (карта местоположений), Линзы (фильтры и эффекты), Истории, Прямые трансляции, Челленджи и игры с друзьями")
 class Facebook(User):
     def __init__(self,id: int,name:str,surname:str) -> None:
          super().__init__(id,name,surname)
-    def features(self) -> None:
-        print(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Публикация текстов, фото и видео, Истории, Группы по интересам, Прямые эфиры, Marketplace для покупок, Лента новостей, Оповещения о событиях")
+    def features(self) -> str:
+        return(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Публикация текстов, фото и видео, Истории, Группы по интересам, Прямые эфиры, Marketplace для покупок, Лента новостей, Оповещения о событиях")
 class Twitter(User):
     def __init__(self,id: int,name:str,surname:str) -> None:
          super().__init__(id,name,surname)
-    def features(self) -> None:
-        print(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Краткие твиты (до 280 символов), Хештеги и тренды, Ответы и ретвиты, Прямые трансляции, Лента новостей, Отслеживание событий через твиты")
+    def features(self) -> str:
+        return(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Краткие твиты (до 280 символов), Хештеги и тренды, Ответы и ретвиты, Прямые трансляции, Лента новостей, Отслеживание событий через твиты")
 class Whatsapp(User):
     def __init__(self,id: int,name:str,surname:str) -> None:
          super().__init__(id,name,surname)
-    def features(self) -> None:
-        print(f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Мгновенные текстовые сообщения, Голосовые и видеозвонки, Групповые чаты, Отправка фото, видео и документов, Статусы, Шифрование сообщений")
+    def features(self) -> str:
+        return (f"Пользователь {self.surname} {self.name} использует {self.network_name}, так как данная соц.сеть имеет ряд функций: Мгновенные текстовые сообщения, Голосовые и видеозвонки, Групповые чаты, Отправка фото, видео и документов, Статусы, Шифрование сообщений")
 
