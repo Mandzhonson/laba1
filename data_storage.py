5cc231b51d464c4b4706d1ca62e016d66116e9e8
manzhos.mihail <manzhos.mihail@mail.ru>
2024-11-19 21:35:34 +0300

    Updates and fixes classes

---
+++ data_storage.py
@@ -0,0 +1,24 @@
+class DataStorage:
+    arr: list[object]
+    id:int
+    def __init__(self) -> None:
+        arr = []
+        id = 1
+    def create(self):
+        pass
+    def read_all(self):
+        pass
+    def read_by_id(self):
+        pass
+    def update(self):
+        pass
+    def delete(self):
+        pass
+    def to_json(self):
+        pass
+    def from_json(self):
+        pass
+    def to_xml(self):
+        pass
+    def from_xml(self):
+        pass
